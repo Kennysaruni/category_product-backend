345c511a38f25205e080fa438f8aa55c4bfe5bf7
Kennysaruni <saruni@DESKTOP-HTVNCDT>
2024-01-24 04:18:09 +0300

    database reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Seed data for categories table
+Category.create(name: 'Electronics')
+Category.create(name: 'Clothing')
+Category.create(name: 'Books')
+Category.create(name: 'Home and Garden')
+# Add more categories as needed
+
+puts 'Seed data for categories has been created.'
